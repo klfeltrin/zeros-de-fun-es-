2a1716cc999372d6ec59b904fc20c33c42550804
unknown <bxnandoxd@hotmail.com>
2020-09-17 21:16:06 -0300

    Versao sem debug

--- estruturaBissesao.js
+++ estruturaBissesao.js
@@ -7,6 +7,66 @@ function binomio() {
 
   binomio.x = $('valorX')
 
+  function insertTable() {
+    element = this.intervalo
+
+    retorno = document.createElement('tr')
+    menor = document.createElement('td')
+    menor.innerText = element[0].i
+    retorno.appendChild(menor)
+    maior = document.createElement('td')
+    maior.innerText = element[1].i
+    retorno.appendChild(maior)
+    media = document.createElement('td')
+    valormedia = (element[0].i + element[1].i) / 2
+    media.innerText = valormedia
+    retorno.appendChild(media)
+    resultado = document.createElement('td')
+    valorresultado = this.calcularbinomio(valormedia)
+    resultado.innerText = valorresultado
+    retorno.appendChild(resultado)
+
+    novointervalo = { retorno: valorresultado, i: valormedia }
+
+    atual = document.getElementById('resultado').getElementsByTagName('tbody')[0]
+    atual.appendChild(retorno)
+
+    if (this.intervalo[0].retorno < 0 && valorresultado < 0) {
+      this.intervalo[0] = novointervalo
+
+    } else {
+      this.intervalo[1] = novointervalo
+    }
+
+    if (valorresultado > ((0, 1) * (Math.pow(10, -3)))) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  function calcularintervalo() {
+    valoresretornados = []
+
+    for (var i = -10; i <= 10; i++) {
+      retorno = this.calcularbinomio(i)
+
+      valoresretornados.push({ retorno, i })
+    }
+
+    for (var i = 0; i < valoresretornados.length - 1; i++) {
+
+      if ((valoresretornados[i].retorno < 0 &&
+        valoresretornados[i + 1].retorno > 0) ||
+        (valoresretornados[i].retorno > 0 &&
+          valoresretornados[i + 1].retorno < 0)
+      ) {
+        this.intervalo = [valoresretornados[i], valoresretornados[i + 1]]
+        return
+      }
+    }
+
+  }
   function setValores() {
     return [
       $(`zero`),
@@ -17,28 +77,38 @@ function binomio() {
       $(`quinto`),
     ]
   }
-  binomio.valores = setValores()
 
-  function calcularbinomio() {
+  function calcularbinomio(x) {
     total = 0
     binomio.valores.map(e = (element, index) => {
-      if(!element.trim())
+      if (!element.trim())
         return
-    
-      total += parseInt(element) * (Math.pow(binomio.x, index))
+
+      total += parseInt(element) * (Math.pow(x, index))
     })
-    console.log(total)
+
+    return (total)
   }
 
   binomio.calcularbinomio = calcularbinomio
+  binomio.calcularintervalo = calcularintervalo
+  binomio.insertTable = insertTable
+
+  binomio.valores = setValores()
 
   return binomio
 }
 
-let a = binomio()
+var a = binomio()
 
-function newBinomio(event) { 
+
+function newBinomio(event) {
   event.preventDefault()
   a = binomio()
-  a.calcularbinomio()
+  a.calcularintervalo()
+  a.insertTable()
+  while (true) {
+
+    if (a.insertTable()) { return }
+  }
 }
